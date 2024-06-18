35b4390e782db5491e0884abe51f4944b6a994ea
bytewired <bu9free@gmail.com>
2024-06-18 15:13:25 +0300

    move files to subfolder

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p build
-
-FILES=(src/main.cpp)
-
-clang++ -std=c++17 -fsanitize=address -Wall -o main ${FILES}
---
+++ mova/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p build
+
+FILES=(src/main.cpp)
+
+clang++ -std=c++17 -fsanitize=address -Wall -o build/main ${FILES}
